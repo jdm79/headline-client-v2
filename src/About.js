d89650442b7fec55464b793dc184b3c78ade6a09
jdm79 <james.malvern@gmail.com>
2020-05-23 21:13:26 +0100

    add a pop-up modal for the about section

--- src/About.js
+++ src/About.js
@@ -9,26 +9,18 @@ export default About => (
         <div className="aboutHeader"> About UK Headline Checker </div>
         <div className="aboutContent">
           {" "}
-          This app updates automatically every five minutes.It's designed to be viewed on a large screen in a newsroom,
+          This app updates automatically every five minutes. It's designed to be viewed on a large screen in a newsroom,
           in order to monitor the national discourse (as directed by the newspapers).
-          It's also viewable on a mobile and iPad, but it's not designed primarily for these devices.
           <br />
            <br />
           It scrapes 16 UK (& Eire) newspapers for their main headlines. 
-          The newspapers are ordered roughly by political alignment, with The Guardian on the left,
-          The Daily Mail on the right. Having the papers positioned like this makes it easier to view
+          The newspapers are ordered roughly by political alignment. Having the papers positioned like this
+          makes it easier to view
           how major stories are spun by different organisations.
            <br />
            <br />
-          The Daily Mirror, Daily Star and Daily Post are all owned by Reach PLC, and they have done
-          a great job trying to stop people scraping their html. Sometimes the API cannot scrape them. 
-          Usually it can though.
-           <br />
-           <br />
-          UK Headline Checker was made with React on the front end, and a Flask Python API on the back end.
-          The web scraper uses the Beautiful Soup library. State management is default React state manager.
-          It's hosted on Heroku on a free plan, so it does take a while to wake up when first loading - 
-          but this is also because the API is making a new scrape of all the national main headlines.
+          The Daily Mirror, Daily Star and Daily Post are all owned by Reach PLC, who have a great dev team 
+          - my scraper sometimes doesn't get past their defences.
         </div>
         <div className="actions">
           
